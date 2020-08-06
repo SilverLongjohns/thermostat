d01eaa99fd7079fe1a21db4d2c604428070c1074
SilverLongjohns <jakehollinger@hotmail.co.uk>
2020-08-06 17:39:53 +0100

    Linked to OpenWeather API

--- interface.js
+++ interface.js
@@ -25,4 +25,25 @@ $(document).ready(function() {
     thermostat.powerSave()
     $("#power-status").text(thermostat.powerSaving);
   });
+
+  var api = "https://api.openweathermap.org/data/2.5/weather?q="
+  var city = "London"
+  var units = "&units=metric"
+  var apiid = "&appid=cd727585689ebbf6618381737fc23e2c"
+
+  $.get( api + city + units + apiid, function( response ){
+  var main = response["main"]  
+  $("#city-temp").text(main["temp"]);
+  });
+
+  $("#cities").change(function() {
+    var api = "https://api.openweathermap.org/data/2.5/weather?q="
+    var units = "&units=metric"
+    var apiid = "&appid=cd727585689ebbf6618381737fc23e2c"
+    var city = $("#cities").val();
+    $.get( api + city + units + apiid, function( response ){
+      var main = response["main"]  
+      $("#city-temp").text(main["temp"]);
+    });
+  });
 });
